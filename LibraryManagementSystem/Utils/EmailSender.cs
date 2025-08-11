2eeccd71c1f5a807623117fa85412c84cd74a390
mohammadsofan <nassersofan@gmail.com>
2025-08-11 17:56:45 +0300

    feat: create Emailsender class and use gmail smtp to send emails

---
+++ LibraryManagementSystem/Utils/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
+
+namespace LibraryManagementSystem.Utils
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var senderEmail = _configuration["EmailSettings:SenderEmail"];
+            var password = _configuration["EmailSettings:Password"];
+            var smtpClient = new System.Net.Mail.SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(senderEmail, password),
+                EnableSsl = true
+            };
+            var mailMessage = new MailMessage(senderEmail!, email, subject, htmlMessage)
+            {
+                IsBodyHtml = true,
+            };
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
