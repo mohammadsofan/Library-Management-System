10831d609eb781082f55d29b663734cf5a3461af
mohammadsofan <nassersofan@gmail.com>
2025-08-29 20:37:37 +0300

    feat: register all application services

---
+++ LibraryManagementSystem/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,88 @@
+﻿using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Interfaces;
+using LibraryManagementSystem.Interfaces.IRepositrories;
+using LibraryManagementSystem.Interfaces.IServices;
+using LibraryManagementSystem.Models;
+using LibraryManagementSystem.Repositories;
+using LibraryManagementSystem.Services;
+using LibraryManagementSystem.Utils;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace LibraryManagementSystem.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = true;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"]!))
+                    };
+                });
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
+            services.AddScoped<IPublisherRepository, PublisherRepository>();
+            services.AddScoped<IBookCopyRepository, BookCopyRepository>();
+            services.AddScoped<ILanguageRepository, LanguageRepository>();
+            services.AddScoped<IReviewRepository, ReviewRepository>();
+            services.AddScoped<ILoanRepository, LoanRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IBookRequestRepository, BookRequestRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IFineRepository, FineRepository>();
+            services.AddScoped<ISystemSettingRepository, SystemSettingRepository>();
+            services.AddScoped<ICityRepository, CityRepository>();
+            services.AddScoped<IBookService, BookService>();
+            services.AddScoped<IAuthorService, AuthorService>();
+            services.AddScoped<IPublisherService, PublisherService>();
+            services.AddScoped<IBookCopyService, BookCopyService>();
+            services.AddScoped<ILoanService, LoanService>();
+            services.AddScoped<ILanguageService, LanguageService>();
+            services.AddScoped<IReviewService, ReviewService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IBookRequestService, BookRequestService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IFineService, FineService>();
+            services.AddScoped<ISystemSettingService, SystemSettingService>();
+            services.AddScoped<ICityService, CityService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IEmailSender, EmailSender>();
+            services.AddScoped<IDBInitializer, DBInitializer>();
+            return services;
+        }
+    }
+}
--- LibraryManagementSystem/Program.cs
+++ LibraryManagementSystem/Program.cs
@@ -1,18 +1,5 @@
-
-using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Extensions;
 using LibraryManagementSystem.Interfaces;
-using LibraryManagementSystem.Interfaces.IRepositrories;
-using LibraryManagementSystem.Interfaces.IServices;
-using LibraryManagementSystem.Models;
-using LibraryManagementSystem.Repositories;
-using LibraryManagementSystem.Services;
-using LibraryManagementSystem.Utils;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace LibraryManagementSystem
 {
@@ -21,46 +8,7 @@ namespace LibraryManagementSystem
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-            {
-                options.User.RequireUniqueEmail = true;
-                options.SignIn.RequireConfirmedEmail = true;
-            })
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                .AddJwtBearer(options =>
-                {
-
-                    options.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SecretKey"]!))
-                    };
-                });
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
-            builder.Services.AddScoped<ITokenService, TokenService>();
-            builder.Services.AddScoped<IEmailSender, EmailSender>();
-            builder.Services.AddScoped<IUserService, UserService>();
-            builder.Services.AddScoped<IDBInitializer, DBInitializer>();
+            builder.Services.AddApplicationServices(builder.Configuration);
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
